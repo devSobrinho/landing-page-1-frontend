9e8de4337127439fd589f7f0a696b8d3ae22c462
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-01-21 19:23:24 -0300

    Added url Heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
-  // url: 'https://strapi-landing-pages-sobrinho.herokuapp.com/pages/?slug=',
-  url: 'http://localhost:1337/pages/?slug=',
+  url: 'https://strapi-landing-pages-sobrinho.herokuapp.com/pages/?slug=',
+  // url: 'http://localhost:1337/pages/?slug=',
   siteName: 'Dev Sobrinho',
   defaultSlug: 'landing-page',
 };
